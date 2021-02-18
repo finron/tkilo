e69e88975ecf608025de76d7a83f87046d0cb041
finron <63902475+finron@users.noreply.github.com>
2021-02-18 10:58:59 +0800

    step3

--- tkilo.go
+++ tkilo.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	fmt.Println("==start==")
+	buf := make([]byte, 1)
+	for b, err := os.Stdin.Read(buf); err == nil && b == 1; b, err = os.Stdin.Read(buf) {
+		fmt.Printf("Resp:=%s \n", buf) // Ctrl - D to reach the end of file // Enter is anther right input char = " ""
+	}
 	os.Exit(0) // echo $?
 }
